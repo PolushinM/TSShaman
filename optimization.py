d172f9ca5235bcbc9a3c4b7e563f418ef008f058
Maksim Polushin <polushinm@yandex.ru>
2022-01-21 23:55:20 +0400

    Deleting of dead code, fixing bug in stack Ridge model

--- optimization.py
+++ optimization.py
@@ -13,40 +13,6 @@ def get_best_l2_alpha(X, y, cv=16, random_state=0):
     return get_best_lr_alpha(X, y, cv=cv, random_state=random_state, penalty='l2')
 
 
-def get_best_lr_alpha_old(X, y, cv=16, random_state=0, penalty='l1'):
-    alphas = [0.005 * 5 ** i for i in range(12)]
-    clf = GridSearchCV(
-        estimator=SGDRegressor(penalty=penalty,
-                               eta0=0.01,
-                               power_t=0.35,
-                               max_iter=10000,
-                               random_state=random_state),
-        param_grid={'alpha': alphas},
-        cv=cv)
-    clf.fit(X, y)
-    # Results of alpha grid search
-    gs_results = clf.cv_results_['mean_test_score']
-    best_result_num = np.argmax(gs_results)
-    best_alpha = alphas[best_result_num]
-
-    alphas = [best_alpha / 4.9 * 1.89 ** i for i in range(6)]
-    clf = GridSearchCV(
-        estimator=SGDRegressor(penalty=penalty,
-                               eta0=0.005,
-                               power_t=0.25,
-                               max_iter=10000,
-                               random_state=random_state),
-        param_grid={'alpha': alphas},
-        cv=cv)
-    clf.fit(X, y)
-
-    # Results of alpha grid search
-    gs_results = clf.cv_results_['mean_test_score']
-    best_result_num = np.argmax(gs_results)
-
-    return alphas[best_result_num], clf.cv_results_['mean_test_score'][best_result_num]
-
-
 def calculate_next_point(X, y, interval, penalty, random_state, cv):
     left_alpha, right_alpha, left_score, right_score = interval
     new_alpha = ((left_alpha ** left_score * right_alpha ** right_score) ** (1 / (left_score + right_score)))
--- sh_linear_model.py
+++ sh_linear_model.py
@@ -134,8 +134,6 @@ class ShLinearModel(ShBaseModel):
                 alpha_multiplier=2,
                 feature_selection_strength=0.0005):
 
-            if additive_features is not None:
-                self.linear_features = additive_features
             self.y = y.copy()
 
             # Generate and join synthetic features
@@ -209,6 +207,7 @@ class ShLinearModel(ShBaseModel):
         self.shift_long_features_obj = LongShiftFeaturesHost(name='y',
                                                              review_period=review_period,
                                                              forecast_period=forecast_period)
+
         self.shift_short_features_obj = ShortShiftFeaturesHost(name='y',
                                                                review_period=review_period,
                                                                forecast_period=forecast_period)
@@ -257,8 +256,7 @@ class ShLinearModel(ShBaseModel):
                                             cv=2).mean())
 
         stack = np.vstack((self.long_linear_model.linear_model.predict(self.long_linear_model.X[self.review_period:]),
-                           self.long_linear_model.linear_model.predict(
-                               self.long_linear_model.X[self.review_period:]))).T
+                           self.short_linear_model.linear_model.predict(self.short_linear_model.X[self.review_period:]))).T
 
         self.stack_model = Ridge(fit_intercept=False, alpha=30).fit(stack, y[self.review_period:])
 
@@ -275,115 +273,6 @@ class ShLinearModel(ShBaseModel):
         y = (self.stack_model.coef_[0] + long_coef) * y_long + (self.stack_model.coef_[1] - long_coef) * y_short
 
         return pd.DataFrame(y[-forecast_period:], index=X_pred.index)
-
-    '''def fit_old(self, X: pd.DataFrame, y: pd.Series,
-            additive_features: list = None,
-            cv=8,
-            verbose=False,
-            elaboration_level=1,
-            alpha_multiplier=2,
-            feature_selection_strength=0.0005):
-
-        if additive_features is not None:
-            self.linear_features = additive_features
-        self.y = y.copy()
-
-        # Generate and join synthetic features
-        self.X = pd.DataFrame(index=X.index) \
-            .join(self.timedata_features_obj.generate(X, y)) \
-            .join(self.shift_long_features_obj.generate(y)) \
-            .join(self.ma_features_obj.generate(y))
-
-        short_features_to_drop, _ = l1_feature_select(self.X[self.review_period:],
-                                                y[self.review_period:],
-                                                strength=feature_selection_strength,
-                                                cv=cv,
-                                                random_state=self.random_state)
-
-        self.X.drop(short_features_to_drop, axis=1, inplace=True)
-
-        self.timedata_features_obj.assign_mask(self.X.columns)
-        self.shift_long_features_obj.assign_mask(self.X.columns)
-        self.ma_features_obj.assign_masks(self.X.columns)
-
-        # Reset dataset, recreate and join synthetic features by masks with additional features
-        self.X = pd.DataFrame(index=X.index) \
-            .join(self.timedata_features_obj.generate(X, y)) \
-            .join(X[self.linear_features]) \
-            .join(self.shift_long_features_obj.generate(y)) \
-            .join(self.ma_features_obj.generate(y))
-
-        self.alpha, _ = get_best_l2_alpha(self.X[self.review_period:],
-                                          y[self.review_period:],
-                                          cv=cv)
-        self.alpha = self.alpha * alpha_multiplier**(3/self.X.shape[1])
-        self.linear_long_model = SGDRegressor(penalty='l2',
-                                              eta0=0.002,
-                                              power_t=0.2,
-                                              max_iter=50000,
-                                              alpha=self.alpha)
-
-        self.linear_long_model.fit(self.X[self.review_period:].values, y[self.review_period:])
-
-
-
-
-        short_features = self.shift_short_features_obj.generate(y)
-        short_features_to_drop, _ = l1_feature_select(short_features[self.review_period:],
-                                                      y[self.review_period:],
-                                                      strength=feature_selection_strength,
-                                                      cv=cv,
-                                                      random_state=self.random_state)
-        short_features.drop(short_features_to_drop, axis=1, inplace=True)
-        self.shift_short_features_obj.assign_mask(short_features.columns)
-
-
-        self.alpha_short, _ = get_best_l2_alpha(short_features[self.review_period:],
-                                                y[self.review_period:],
-                                                cv=cv)
-        self.alpha_short = self.alpha_short * alpha_multiplier**(16/short_features.shape[1])
-        short_features = self.shift_short_features_obj.generate(y)
-        self.linear_short_model = SGDRegressor(penalty='l2',
-                                               eta0=0.002,
-                                               power_t=0.2,
-                                               max_iter=50000,
-                                               alpha=self.alpha_short)
-        self.linear_short_model.fit(short_features[self.review_period:].values, y[self.review_period:])
-        print('Short R^2=', cross_val_score(self.linear_short_model,
-                                            short_features[self.review_period:],
-                                            y[self.review_period:],
-                                            cv=2).mean())
-
-        stack = np.vstack((self.linear_long_model.predict(self.X[self.review_period:].values),
-                          self.linear_short_model.predict(short_features[self.review_period:]))).T
-        self.stack_model = Ridge(fit_intercept=False, alpha=1000).fit(stack, y[self.review_period:])
-        print(self.stack_model.coef_)
-
-        return self
-
-    def predict_old(self, X=pd.DataFrame(), forecast_period=1, verbose=False):
-        long_coef = 0.1
-        y = self.y.values
-        X_pred = self.generate_empty_predict_frame(forecast_period)
-        X_pred = X_pred.join(self.timedata_features_obj.generate(X_pred, y=None, calculate_weights=False)) \
-            .join(X[self.linear_features])
-
-        for i in range(0, forecast_period):
-            y = np.append(y, (self.stack_model.coef_[0]+long_coef)*self.predict_one_step_long(X_pred.iloc[i].values, y) +
-                          (self.stack_model.coef_[1]-long_coef)*self.predict_one_step_short(y))
-
-        return pd.DataFrame(y[-forecast_period:], index=X_pred.index)
-        
-        
-    def predict_one_step_long(self, X_pred_row: np.array, y: np.array):
-        shift_row = self.shift_long_features_obj.get_one_row(data=y)
-        indicators_row = self.ma_features_obj.get_one_row(y=y[-1])
-        return self.linear_long_model.predict(np.hstack((X_pred_row, shift_row, indicators_row)).reshape(1, -1))
-
-    def predict_one_step_short(self, y: np.array):
-        shift_row = self.shift_short_features_obj.get_one_row(data=y)
-        return self.linear_short_model.predict(shift_row.reshape(1, -1))'''
-
 
     def generate_empty_predict_frame(self, forecast_period):
         step_time = self.step_time
